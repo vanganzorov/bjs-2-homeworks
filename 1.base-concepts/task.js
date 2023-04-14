c0f9b8293d6d3f0f56b1e04b3a2aeadf50425faf
Vladimir Anganzorov <vanganzorov@gmail.com>
2023-04-14 12:50:54 +0700

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
+
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+
+  let d;
+  d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d < 0) {
+      return [];
+  }
+
+  else if(d === 0) {
+      return [-b / (2 * a)];
+  } else if (d > 0) {
+      return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+  let P = percent / 100 / 12;
+  let S = amount - contribution;
+
+  let payments = S * (P + (P / (((1 + P) ** countMonths) - 1)));
+
+  return Number(parseFloat(payments * countMonths).toFixed(2));
 }
